78f0ec5ea6323e8518b663872183a664fe3e5437
adityagupta19 <adityagupta11219@gmail.com>
2025-01-03 00:28:58 +0530

    added: simple frontend logic to make the api call

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import axios from 'axios';
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [url, setUrl] = useState<string>("")
+  const [response,setResponse] = useState<any>();
+
+  const apiEndpoint = "http://localhost:3000/api/v1"
+
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setUrl(e.currentTarget.value);
+  };
+  const handleSubmit = async () => {
+    const request = {
+      "url": url,
+      "short":"",
+      "expiry":0 
+    }
+    console.log("api call is being made: ",request);
+    const res = await axios.post(apiEndpoint,request);
+    setResponse(res)
+  }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Shorten your url</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <input 
+          placeholder='What do you want to shorten???'
+          type='text'
+          name='url'
+          value={url}
+          onChange={handleChange}
+        ></input>
+        <button onClick={handleSubmit} color='#000000'> Shorten </button>
+        {response.short}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
