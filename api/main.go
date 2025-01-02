7f9eec7d30d48a9574b1650dafb48be6729ec42a
adityagupta19 <adityagupta11219@gmail.com>
2025-01-03 00:59:24 +0530

    enabled cors globally fr this time

--- api/main.go
+++ api/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/adityagupta19/url_shortner/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
 )
@@ -25,6 +26,7 @@ func main() {
 
 	app := fiber.New()
 	app.Use(logger.New())
+	app.Use(cors.New())
 
 	setupRoutes(app)
 
